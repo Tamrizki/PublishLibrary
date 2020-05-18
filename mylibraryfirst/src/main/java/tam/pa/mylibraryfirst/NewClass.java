c540fedd340ee1963fa3cf8360efbf3e5e7dafe4
tamrizki <rizkitam77@gmail.com>
2020-05-18 09:25:47 +0700

    first commit

---
+++ mylibraryfirst/src/main/java/tam/pa/mylibraryfirst/NewClass.java
@@ -0,0 +1,10 @@
+package tam.pa.mylibraryfirst;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class NewClass {
+    public static void Toas(Context ctx){
+        Toast.makeText(ctx, "Cek", Toast.LENGTH_SHORT).show();
+    }
+}
